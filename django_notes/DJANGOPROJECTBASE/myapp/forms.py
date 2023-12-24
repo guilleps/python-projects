dee34f622db0e0a4586f6efd09ebabfb5e28eef3
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2023-12-24 16:34:04 -0500

    learn django

--- django_notes/DJANGOPROJECTBASE/myapp/forms.py
+++ django_notes/DJANGOPROJECTBASE/myapp/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.CharField(label="Titulo de tarea", max_length=200)
-    description = forms.CharField(label="descripcion de la tarea", widget=forms.Textarea)
+    title = forms.CharField(label="Titulo de tarea", 
+                            max_length=200)
+    description = forms.CharField(label="descripcion de la tarea")
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label="Name project", max_length=200)
+    name = forms.CharField(label="Name project", 
+                           max_length=200)
