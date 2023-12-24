56aeaacf0fab0c6a33791fa844e3cc992ea29eff
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2023-12-24 10:55:28 -0500

    Add params

--- django_notes/DJANGOPROJECTBASE/myapp/urls.py
+++ django_notes/DJANGOPROJECTBASE/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
+    path('', views.index),
+    path('hello/<str:username>/<int:id>', views.hello),
     path('about/', views.about)
 ]
--- django_notes/DJANGOPROJECTBASE/myapp/views.py
+++ django_notes/DJANGOPROJECTBASE/myapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('Hello World')
+def index(request):
+    return HttpResponse('Index')
+
+def hello(request, username, id):
+    return HttpResponse('Hello %s y %i' % (username,id))
 
 def about(request):
     return HttpResponse('About')
