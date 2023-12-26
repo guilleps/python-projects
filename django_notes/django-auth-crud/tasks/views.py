6491e648e6d926282e87ebcac461fe7b78dfdf5f
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2023-12-25 23:19:35 -0500

    Add logout

--- django_notes/django-auth-crud/tasks/views.py
+++ django_notes/django-auth-crud/tasks/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.db import IntegrityError
 
 # Create your views here.
-
-
 def home(request):
     return render(request, 'home.html')
 
@@ -23,8 +22,9 @@ def signup(request):
                 user = User.objects.create_user(
                     username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created successfully')
-            except:
+                login(request, user)    # crea una sesión en el navegador
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm(),
                     'error': 'Username already exists'
@@ -36,3 +36,7 @@ def signup(request):
     
 def tasks(request):
     return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
